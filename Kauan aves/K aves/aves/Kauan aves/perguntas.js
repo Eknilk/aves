ea7077529a20d1ff309583ee22f0b1e7dc716a32
Eknilk <00001120724624sp@al.educacao.sp.gov.br>
2024-11-21 14:18:48 -0300

    Add files via upload

---
+++ Kauan aves/K aves/aves/Kauan aves/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ave Pet.',
+    'O que é um Ring Neck?',
+    'O Ring Neck é um tipo de periquito, originário da África e Ásia, conhecido pelo anel colorido em volta do pescoço.'
+)
+
+criaCartao(
+    'Pouco conhecido.',
+    'O que é uma Rosela?',
+    'É um tipo de papagaio australiano, colorido de cauda longa, e pode ser uma ave de estimação.'
+)
+
+criaCartao(
+    'Diversas mutações.',
+    'O que é um Red Rumped?',
+    'É um periquito australiano com diversas mutações'
+)
+
+criaCartao(
+    'Lingua Inglesa.',
+    'Como se fala pássaro em ingles',
+    'Pássaro em ingles é Bird '
+)
